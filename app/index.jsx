dac30d0ecb95995ba626dd7477d509c8833feea2
WHS-Practicum-of-IT <whspracticumofit@gmail.com>
2024-11-01 09:26:08 -0500

    added sign-in and sign-up pages

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router'
+import { Redirect, router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Text className="text-3xl font-pblack text-red-800">WHS Maps</Text>
-          <StatusBar style="auto" />
-          <Link href="/home" style={{ color: 'grey' }}>Go to Home</Link>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   );
 }
